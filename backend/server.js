aef32bf3860bd2d37f4df9df5bb9358c3c71678a
Avinash <avinash2596360@gmail.com>
2026-02-08 01:34:46 +0530

    backend added

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express  = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// Middleware to handle Cors
+app.use(
+    cors({
+        origin:"*",
+        methods:["GET","POST","PUT","DELETE"],
+        allowedHeaders:["Content-Type","Authorization"],
+
+    })
+);
+
+// Connect database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+// app.use("/api/auth",authRoutes);
+
+// Serve uploads folder
+app.use("/uploads",express.static(path.join(__dirname,"uploads"),{}));
+
+// Start Server
+const PORT =process.env.PORT || 5000;
+app.listen(PORT,() => console.log('Server is running on port ${PORT}'));
